9ab8b2e643e9c75733d77ef1d206ff70967d896a
Cecilia Parrish <44381309+CeciliaCodes@users.noreply.github.com>
2021-02-16 17:51:34 -0600

    post and get requests
    added more to the server.index file

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -11,9 +11,12 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+
+
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -26,6 +29,27 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
 
+
+app.post('/analyze', function(req, res){
+    const API_KEY = process.env.API_KEY;
+    const articleUrl = req.body.articleUrl;
+
+    const baseUrl = "https://api.meaningcloud.com/sentiment-2.1";
+    const params = `?key=${API_KEY}&lang=en&model=general&url=${articleUrl}`;
+    const urlToFetch = baseUrl + params;
+
+
+    fetch(urlToFetch, {
+        method: 'POST',
+        headers: { 
+            "content-type": "application/JSON",
+        }
+
+    })
+
+
+    //res.send(data)
+})
 
 //Declaring API credentials
 
